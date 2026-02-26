18fe6a42d76eadd2267c669b37d3d47de6da1180
bunnystring <ziknaf_548@hotmail.com>
2026-02-26 00:40:22 -0500

    feat(issue-13): Se implemenetan detalle de la orden y se refactoriza consultas largas aservicios  batch

--- src/app/modules/private/employees/pages/employees-detail/employees-detail.component.ts
+++ src/app/modules/private/employees/pages/employees-detail/employees-detail.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employees-detail',
   templateUrl: './employees-detail.component.html',
-  styleUrls: ['./employees-detail.component.css']
+  styleUrls: ['./employees-detail.component.css'],
 })
 export class EmployeesDetailComponent implements OnInit {
+  constructor(private location: Location) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  /**
+   * Navega a la página anterior utilizando el servicio Location de Angular
+   * Permite al usuario volver a la lista de dispositivos o a la página desde donde accedió al detalle del dispositivo
+   * @returns void
+   */
+  goBack(): void {
+    this.location.back();
   }
-
 }
--- src/app/modules/private/groups/pages/groups-detail/groups-detail.component.ts
+++ src/app/modules/private/groups/pages/groups-detail/groups-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-groups-detail',
   templateUrl: './groups-detail.component.html',
-  styleUrls: ['./groups-detail.component.css']
+  styleUrls: ['./groups-detail.component.css'],
 })
 export class GroupsDetailComponent implements OnInit {
+  constructor(
+    private location: Location,
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  /**
+   * Navega a la página anterior utilizando el servicio Location de Angular
+   * Permite al usuario volver a la lista de dispositivos o a la página desde donde accedió al detalle del dispositivo
+   * @returns void
+   */
+  goBack(): void {
+    this.location.back();
   }
-
 }
